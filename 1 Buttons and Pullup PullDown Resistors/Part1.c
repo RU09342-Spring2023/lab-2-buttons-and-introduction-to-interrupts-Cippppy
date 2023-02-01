43b70a382c74ed108b81790f5c7ac4b21de732f1
Cippppy <98924381+Cippppy@users.noreply.github.com>
2023-02-01 12:46:18 -0500

    Revert "Initialized CCS project, finished Part 1"
    This reverts commit 2dae3dbe2702e283c49a5d56531cf9a2d00785c1.

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -32,9 +32,9 @@ int main(void)
 
     while(1)
     {
-        if (P2IN ^ BIT3)            // If S2 (P2.3) is pressed
+        if (P2IN & BIT3)            // If S2 (P2.3) is pressed
             P6OUT ^= BIT6;          // Toggle P6.6
-        if (!(P4IN & BIT1))            // If S1 (P4.1) is pressed
+        if (P4IN & BIT1)            // If S1 (P4.1) is pressed
             P1OUT ^= BIT0;          // Toggle P1.0
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
     }
@@ -49,10 +49,6 @@ void gpioInit()
 
        P1DIR |= BIT0;              // Configure P1.0 to an Output
        P6DIR |= BIT6;              // Configure P6.6 to an Output
-
-       P1OUT &= ~BIT0;             // Clear P1.0 output latch for a defined power-on states
-       P6OUT &= ~BIT6;             // Clear P6.6 output latch for a defined power-on states
-
        P2DIR &= ~BIT3;             // Configure P2.3 to an Input
        P4DIR &= ~BIT1;             // Configure P4.1 to an Input
 
@@ -70,5 +66,4 @@ void gpioInit()
 
        P4REN |= BIT1;               // Enable Resistor on P4.1
        P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
-
 }
